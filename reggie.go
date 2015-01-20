8117b493d95d200f6f6205dc408c63470cecb69b
Nick Maloney <ngmaloney@gmail.com>
2015-01-20 09:14:32 -0500

    * Add flags for verbose output and dry-run. * Use flag.Arg instead of os.Args

--- reggie.go
+++ reggie.go
@@ -8,67 +8,75 @@ import "io"
 import "flag"
 
 func main() {
-	regex := os.Args[1]
-	src := os.Args[2]
-	dest := os.Args[3]
-	dryRun := flag.Bool("dry-run", false, "Dry run, don't copy files.")
+  dryRun := flag.Bool("dry-run", false, "Dry run, don't actually copy files.")
+  verbose := flag.Bool("verbose", false, "Verbose output, print list of files copied.")
+  fileCount := 0
 
-	flag.Parse()
+  flag.Parse()
 
-	if(len(regex) == 0) {
-		fmt.Println("Valid regex argument is required")
-		os.Exit(1)
-	}
+  regex := flag.Arg(0)
+  src := flag.Arg(1)
+  dest := flag.Arg(2)
+
+
+  if(len(regex) == 0) {
+    fmt.Println("Invalid regex param")
+    os.Exit(1)
+  }
 
   src_fi, src_err := os.Stat(src)
-	if (len(src) == 0 || src_err != nil || !src_fi.IsDir()) {
-		fmt.Println("Valid source directory is required")
-		os.Exit(1)
+  if (len(src) == 0 || src_err != nil || !src_fi.IsDir()) {
+    fmt.Println("Reggie: Invalid source directory.")
+    os.Exit(1)
   }
 
   dest_fi, dest_err := os.Stat(dest)
-	if (len(dest) == 0 || dest_err != nil || !dest_fi.IsDir()) {
-		fmt.Println("Valid destination directory is required")
-		os.Exit(1)
-	}
-
-	callback := func(path string, fi os.FileInfo, err error) error {
-		match, err := regexp.MatchString(regex, path)
-		if(match) {
-			fmt.Println("Copying: " + path)
-			if(!*dryRun) {
-				err := CopyFile(path, dest)
-				if(err != nil) {
-					fmt.Println(err)
-				}
-			}
-		}
-		return err
+  if (len(dest) == 0 || dest_err != nil || !dest_fi.IsDir()) {
+    fmt.Println("Reggie: Invalid destination directory.")
+    os.Exit(1)
   }
 
-	filepath.Walk(src, callback)
-	if(*dryRun) {
-		fmt.Println("Dry run complete")
-	}
+  callback := func(path string, fi os.FileInfo, err error) error {
+    match, err := regexp.MatchString(regex, path)
+    if(match) {
+      fileCount++
+      if(*verbose) {
+        fmt.Println("Copying: " + path)
+      }
+      if(!*dryRun) {
+        err := CopyFile(path, dest)
+        if(err != nil) {
+          fmt.Println(err)
+        }
+      }
+    }
+    return err
+  }
+
+  filepath.Walk(src, callback)
+  fmt.Printf("Copied %v files\n", fileCount)
+  if(*dryRun) {
+    fmt.Println("Dry run complete")
+  }
 }
 
 func CopyFile(csrc string, cdest string) error {
-	s, err := os.Open(csrc)
-	if err != nil {
-		return err
-	}
+  s, err := os.Open(csrc)
+  if err != nil {
+    return err
+  }
 
-	fd_base := filepath.Base(csrc)
-	fd_dest := cdest + "/" + fd_base
+  fd_base := filepath.Base(csrc)
+  fd_dest := cdest + "/" + fd_base
 
-	d, err := os.Create(fd_dest)
-	if err != nil {
-		return err
-	}
+  d, err := os.Create(fd_dest)
+  if err != nil {
+    return err
+  }
 
-	if _, err := io.Copy(d, s); err != nil {
-		d.Close()
-		return err
-	}
-	return d.Close()
+  if _, err := io.Copy(d, s); err != nil {
+    d.Close()
+    return err
+  }
+  return d.Close()
 }
